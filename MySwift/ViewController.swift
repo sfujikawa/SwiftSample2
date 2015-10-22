790305cd39102e0ee0a792ccfe4348764b2a2a96
sfujikawa <sfujikawa.itfor@gmail.com>
2015-10-22 11:50:40 +0900

    Initial Commit

---
+++ MySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySwift
+//
+//  Created by sunfuji on 2015/10/22.
+//  Copyright © 2015年 sunfuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
