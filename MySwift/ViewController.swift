d9849e13a91610254409ca5e03c44b9ee355483e
sfujikawa <sfujikawa.itfor@gmail.com>
2015-10-22 13:50:45 +0900

    入門
    10分で試せる！ Swiftを使った初めてのiOSアプリ開発入門
    http://qiita.com/cheeeeese/items/a1f7764b8bfd97c1e6cd

--- MySwift/ViewController.swift
+++ MySwift/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func tapHandler(sender: AnyObject) {
+        myTextField.text = "変更されました";
+    }
+    
+    @IBOutlet weak var myTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
